6f8fb95af02cada08249c969f86193790659c907
Ajith2407 <ajith9799kumar@gmail.com>
2023-05-24 16:52:27 +0530

    name popup appearing

--- script.js
+++ script.js
@@ -6,14 +6,19 @@
             var password = document.getElementById("password").value;
 
             // Set the correct username and password
-            var correctUsername = "admin";
-            var correctPassword = "0123";
+            var correctUsername = "kishore";
+            var correctPassword = "4567";
             var correctUsername2 = "ajith";
             var correctPassword2 = "kumar";
 
             // Check if the entered username and password match the correct credentials
             if ((username === correctUsername && password === correctPassword) || (username === correctUsername2 && password === correctPassword2)) {
-                alert("hi Admin welcome welcome to ksv");
+               if(username === correctUsername && password === correctPassword){
+                alert("Hi Kishore welcome  to ksv");
+               }
+               else{
+                alert("Hi Ajith welcome to ksv");
+               }
               window.open("HomePage.html");
                }
             else {
